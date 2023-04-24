13b3c8c2caa45e3b2ccb66b5b520fa1bdf5e751d
Robbie <robbiethewadley@gmail.com>
2023-04-24 11:24:17 -0600

    new config base

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 module.exports = {
-  plugins: [
-    "@rjwadley/format-gsap",
-    "@typescript-eslint",
-    "function-component-export",
-    "listeners",
-    "react",
-    "sort-styled-components",
-    "ssr-friendly",
-    "styled-components-a11y",
-  ],
   extends: [
-    /**
-     * The base for this config is airbnb
-     */
-    "airbnb",
-    "airbnb/hooks",
-    "airbnb-typescript",
     /**
      * Bring in @typescript-eslint rules and react rules for TS checking
      */
@@ -25,60 +9,51 @@ module.exports = {
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:@typescript-eslint/strict",
     /**
+     * The base for this config is alloy
+     */
+    "alloy",
+    "alloy/react",
+    "alloy/typescript",
+    /**
      * and some utility plugins with extra rules
      */
-    "plugin:ssr-friendly/recommended", // no window access
+    "adjunct", // several useful rules
+    "plugin:compat/recommended", // browser compatibility
+    "plugin:jsx-a11y/recommended", // a11y for react elements
     "plugin:listeners/strict", // strict event listeners
+    "plugin:prettier/recommended", // disable any pure style rules (of which there should be none)
+    "plugin:ssr-friendly/recommended", // no window access
     "plugin:styled-components-a11y/recommended", // a11y for styled components
-    "plugin:prettier/recommended", // disable any pure style rules
   ],
+  plugins: [
+    "@typescript-eslint",
+    "compat",
+    "function-component-export",
+    "jsx-a11y",
+    "listeners",
+    "prettier",
+    "react",
+    "react-hooks",
+    "sort-styled-components",
+    "ssr-friendly",
+    "styled-components-a11y",
+  ],
+  env: {
+    browser: true,
+  },
   rules: {
     /**
      *  Mandatory Rules
      */
 
-    // tighter, more legible formatting for GSAP functions
-    "@rjwadley/format-gsap/compact-functions": "error",
-    // prettier will conflict with gsap formatter
-    "prettier/prettier": "off",
-
     // sort styled components based on their usage order
     "sort-styled-components/sort-styled-components": "warn",
-    // complementary rule to sort styled components (remove extra space)
-    "no-multiple-empty-lines": "warn",
-
-    // we define styled components at the bottom, which is better for readability but incompatible with this rule
-    "@typescript-eslint/no-use-before-define": "off",
 
     // any custom hooks that take dependencies need to be specified here
     "react-hooks/exhaustive-deps": [
       "error",
       {
-        additionalHooks: "useAnimation",
-      },
-    ],
-
-    // this rule has issues with TS
-    "react/require-default-props": "off",
-
-    // import order
-    "import/order": [
-      "warn",
-      {
-        groups: ["builtin", "external", "internal"],
-        pathGroups: [
-          {
-            pattern: "react",
-            group: "external",
-            position: "before",
-          },
-        ],
-        pathGroupsExcludedImportTypes: ["react"],
-        "newlines-between": "always",
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
+        additionalHooks: "useAnimation|useDeepCompareEffect|useDeepCompareMemo",
       },
     ],
 
@@ -95,32 +70,60 @@ module.exports = {
     // ban bad types
     "@typescript-eslint/no-explicit-any": "error",
 
+    // disallow shadowing
+    "@typescript-eslint/no-shadow": "error",
+
     // consistent export style
     "function-component-export/ban-FC": "error",
     "function-component-export/combine-default-export": "error",
 
-    // allow expressions in fragments
-    "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
+    // allow warn, error, and info in console
+    // leave this disabled locally, but enable it in CI
+    "no-console": process.env.CI
+      ? ["error", { allow: ["error", "warn", "info"] }]
+      : "off",
 
-    // allow console.error
-    "no-console": ["error", { allow: ["error"] }],
-
-    // check for keys when needed
-    "react/jsx-key": ["error", { checkFragmentShorthand: true }],
-
-    // disable rules that typescript handles
+    // disable rules that typescript fully handles on its own
     "react/jsx-props-no-spreading": "off",
     "react/no-unknown-property": "off",
+    "no-undef": "off",
+    "unicorn/no-array-callback-reference": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "unicorn/no-useless-undefined": ["error", { checkArguments: false }],
+
+    // this rule prevents us from type checking elements
+    "xss/no-mixed-html": "off",
+
+    // disable secrets and PII rules (they don't really apply to us)
+    "no-secrets/no-secrets": "off",
+    "pii/no-phone-number": "off",
+    "pii/no-email": "error",
+    "pii/no-dob": "warn",
 
     /**
-     *  Optional Rules
+     *  Optional or Temporary Rules
      */
-    "consistent-return": "off",
+
+    // we can remove this once TS 5.1 drops
+    "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
+
+    // we need more time for top-level await to be supported in browsers
+    "unicorn/prefer-top-level-await": "off",
+
+    // the following rules seem annoying, so I've loosened them up (that way they can at least stay on)
+    // the goal should probably be to decrease these over time
+    complexity: ["warn", 40],
+    "sonarjs/cognitive-complexity": ["warn", 40],
+    "max-params": ["warn", 6],
+
+    // similar story with these, but I've entirely disabled them
     "no-nested-ternary": "warn",
+    "consistent-return": "off",
+    "const-case/uppercase": "off",
+    "eslint-comments/disable-enable-pair": "off",
+    "scanjs-rules/identifier_localStorage": "off",
+    "scanjs-rules/assign_to_src": "off",
   },
-  /**
-   * parser setup
-   */
   parserOptions: {
     project: ["./tsconfig.json"],
   },
@@ -128,8 +131,8 @@ module.exports = {
     "import/resolver": {
       typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
     },
+    react: {
+      version: "detect",
+    },
   },
-
-  // ignore the public folder
-  ignorePatterns: "public/*",
 };
